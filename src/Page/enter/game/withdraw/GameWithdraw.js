e68ecb58f5d1fb2c6d593a74b4d1bc33008f741e
Benny Liew Wai Keat <benny.liew@pactmail.com>
2019-09-09 16:22:31 +0800

    【bug】输入取款密码时只显示7个数字

--- src/Page/enter/game/withdraw/GameWithdraw.js
+++ src/Page/enter/game/withdraw/GameWithdraw.js
@@ -141,7 +141,7 @@ export default class GameWithdraw extends Component {
                             <TCText borderRadius={5}
                                     backgroundStyle={{
                                         backgroundColor: "rgb(209,212,230)", paddingHorizontal: SCREEN_W * 0.07,
-                                        paddingVertical: 2, left: leftPosition + 110, top: SCREEN_H * 0.38, width: 260
+                                        paddingVertical: 2, left: leftPosition + 110, top: SCREEN_H * 0.38, width: 272
                                     }}
                                     textStyle={{color: "#353535", fontSize: 15}}
                                     text={this.onFormatBank(itemData.bank.bankCardNo)}
