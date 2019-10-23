d70b271a3645edb4234355c9693c1805e645807c
Sam <sam@SamdeMac-mini.local>
2019-10-23 11:49:40 +0800

    autoMerge-releae and replace config date: 10-23 11:49

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -2,18 +2,19 @@ import React, {
     Platform
 } from 'react-native';
 
+
 export const appDomainBase = {
-    base1: 'https://qp01-game.513xyz.com',
-    base2: 'https://qp01-game.513xyz.com,',
-    base3: 'https://qp01-game.513xyz.com',
-    base4: 'https://qp01-game.513xyz.com',
-    base5: 'https://qp01-game.513xyz.com',
-    base6: 'https://qp01-game.513xyz.com',
-    base7: 'https://qp01-game.513xyz.com',
+    base1: "https://ml9kmychyqp.wxzbk.com",
+    base2: "https://xw3yzuchyqp.wxzbk.com",
+    base3: "https://cfvp4uchyqp.wxzbk.com",
+    base4: "https://0lfzqvchyqp.wxzbk.com",
+    base5: "https://ml9kmychyqp.qingfengzdh.com",
+    base6: "https://xw3yzuchyqp.qingfengzdh.com",
+    base7: "https://xw3yzuchyqp.qingfengzdh.com",
 }
 
 
-export let configAppId = "214"
+export let configAppId = "1147"
 
 
 export const AppConfig = {
@@ -36,35 +37,27 @@ export const AppConfig = {
 }
 
 
-export const MyAppName = '博博乐';
+export const MyAppName = '超会赢棋牌';
+export const MyOwnerPlatName= '超会赢棋牌';
 
-export const versionHotFix = 'v8.27';
+export const versionHotFix = 'v7.22';
 
-export const MyOwnerPlatName= '博博乐';
-
-// export const safeguardDomain = [
-//     'https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com',
-//     'https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com']
-
-
-//base64key 格式示例
-// {"d":["https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com","https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com"]}
 
 //第三方安全域名base64 key
-export const safeguardKey = 'eyJkIjogWyJodHRwczovLzk4NzY0NWJhMDBhOWIwNDE2YjI1NGYzM2Q5MThlZDY0Lm9zcy1jbi1zaGVuemhlbi5hbGl5dW5jcy5jb20iLCAiaHR0cHM6Ly9jNDBiNmUzZDY2NDU1NmFiNDIzZDNlZWJjMDFhYjJmZC5vc3MtY24tc2hlbnpoZW4uYWxpeXVuY3MuY29tIl19'
+export const safeguardKey = 'ewoJImQiOiBbImh0dHBzOi8vY2VhNGVjYTY1MGQ3MWJkOWQ1NTJiNGNmMzhlMDU3MDIub3NzLWNuLXNoZW56aGVuLmFsaXl1bmNzLmNvbSIsCgkJImh0dHBzOi8vY2VhNGVjYTY1MGQ3MWJkOWQ1NTJiNGNmMzhlMDU3MDIuczMtYWNjZWxlcmF0ZS5hbWF6b25hd3MuY29tIiwKCQkiaHR0cHM6Ly9jZWE0ZWNhNjUwZDcxYmQ5ZDU1MmI0Y2YzOGUwNTcwMi5henVyZWVkZ2UubmV0IgoJXQp9'
+
 
 export const platInfo = {
 
-    downDomain:"https://download.jinkuangjia.com",
+    downDomain:"https://download1.jinkuangjia.com",
 
     zipCheckServer: {
         debug_server: "http://192.168.14.70:8888",
-        release_server: "/game/release/uat"
+        release_server: "/game/release/chyqp",
     },
+    latestNativeVersion:{ios:"6.0",android:"7.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
     platId: configAppId,
-    brand: "qp01",
-    latestNativeVersion:{ios:"10",android:"10"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
+    brand: "chyqp",
 }
-
 
 
