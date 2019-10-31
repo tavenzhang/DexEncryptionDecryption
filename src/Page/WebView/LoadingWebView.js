4c5885d5eb06fd26160bc9235ce6abdd0d87869b
taven <abcd1234>
2019-10-31 16:32:57 +0800

    [bug]检测版本调整优化

--- src/Page/WebView/LoadingWebView.js
+++ src/Page/WebView/LoadingWebView.js
@@ -43,7 +43,7 @@ export default class LoadingWebView extends Component {
 
         //let visible = TW_Store.gameUpateStore.isNeedUpdate||TW_Store.gameUpateStore.isAppDownIng
         TW_Log("targetAppDir----LoadingWebView-isNeedUpdate=="+TW_Store.gameUpateStore.isNeedUpdate+"---isAppDownIng="+TW_Store.gameUpateStore.isAppDownIng);
-        let visible = TW_Store.gameUpateStore.isNeedUpdate
+        let visible = TW_Store.gameUpateStore.isNeedUpdate ||TW_Store.gameUpateStore.isLoading
         if(!visible){
             return null;
         }
