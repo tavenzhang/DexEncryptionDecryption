59e39b1accab39aad0aa79d38d9b7e0241f41409
Sam <sam@SamdeMac-mini.local>
2019-11-01 14:30:19 +0800

    autoMerge-releae and replace config date: v11.01.1430

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -37,13 +37,10 @@ export const AppConfig = {
 }
 
 
-export const MyAppName = '博博乐';
+export const MyAppName = '超会赢棋牌';
+export const MyOwnerPlatName= '超会赢棋牌';
 
-export const versionHotFix ='v11.01.1343'
-
-export const MyOwnerPlatName= '博博乐';
-
-export const versionHotFix ='v11.01.1343'
+export const versionHotFix ='v11.01.1430'
 
 
 //第三方安全域名base64 key
