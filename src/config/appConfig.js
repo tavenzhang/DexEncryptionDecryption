697c1b9442a4c1a5d4d292f82aed47d95fe105e9
Sam <sam@SamdeMac-mini.local>
2019-11-01 13:43:25 +0800

    autoMerge-releae and replace config date: v11.01.1343

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -38,7 +38,7 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐';
 
-export const versionHotFix ='v11.01.1307'
+export const versionHotFix ='v11.01.1343'
 
 export const MyOwnerPlatName= '博博乐';
 
