f0c33f80e35f5f4fd2f14c5c7c76f41f85df94a0
taven <abcd1234>
2019-11-08 16:13:04 +0800

    [bug] 完善游戏退出

--- src/Data/store/BBLStore.js
+++ src/Data/store/BBLStore.js
@@ -143,7 +143,7 @@ export  default  class BBLStore {
         }
         if (TW_OnValueJSHome) {
             TW_OnValueJSHome(TW_Store.bblStore.getWebAction(TW_Store.bblStore.ACT_ENUM.appData, {isAtHome: true}));
-            TW_OnValueJSHome(TW_Store.bblStore.getWebAction(TW_Store.bblStore.ACT_ENUM.lobbyResume),{...message});
+            TW_OnValueJSHome(TW_Store.bblStore.getWebAction(TW_Store.bblStore.ACT_ENUM.lobbyResume,{...message}));
         }
         setTimeout(()=>{ TW_Store.gameUpateStore.isInSubGame = false;},G_IS_IOS ? 500:800)
     }
