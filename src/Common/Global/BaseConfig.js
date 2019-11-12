7aa8d0e7a50cc095a1ef37621a4e5a4514d0cb12
taven <abcd1234>
2019-11-12 20:55:26 +0800

    [bug]优化大厅的加载

--- src/Common/Global/BaseConfig.js
+++ src/Common/Global/BaseConfig.js
@@ -22,7 +22,10 @@ global.TW_LoaderOnValueJS=()=>{};
 global.TW_OnValueJSSubGame=()=>{};
 global.TW_OnBackHomeJs=null;
 global.TW_SplashScreen_HIDE=()=>{
-    SplashScreen.hide();
+    setTimeout(()=>{
+        SplashScreen.hide();
+    },1000)
+
     // if(Orientation&&Orientation.lockToLandscapeLeft()){
     //     Orientation.lockToLandscapeLeft();
     // }
