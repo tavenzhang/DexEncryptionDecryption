74c0f13d7e2c98e5da78c8360fadf600b5e79c1c
taven <abcd1234>
2019-11-18 14:49:22 +0800

    [bug]整合sit 最新调整

--- src/Page/web/XXWebView.js
+++ src/Page/web/XXWebView.js
@@ -514,6 +514,7 @@ export default class XXWebView extends Component {
     onLoadEnd = () => {
         TW_Store.bblStore.isLoading = false;
         this.onEvaleJS(TW_Store.bblStore.getWebAction(TW_Store.bblStore.ACT_ENUM.windowResize, {}));
+        TW_Store.bblStore.changeShowDebug(true);
     }
 
 
