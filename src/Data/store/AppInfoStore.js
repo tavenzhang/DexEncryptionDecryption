8f0663c5b547a9d11ab568c5810c6d9078c74d67
taven <abcd1234>
2019-11-20 11:37:37 +0800

    [bug]优化加载下载

--- src/Data/store/AppInfoStore.js
+++ src/Data/store/AppInfoStore.js
@@ -152,10 +152,12 @@ export default class AppInfoStore {
             }
 
             TW_Data_Store.getItem(TW_DATA_KEY.LobbyReadyOK, (err, ret) => {
-                // if(TW_Store.dataStore.isAppInited){
-                //     TW_Store.gameUpateStore.isNeedUpdate=`${ret}` == "1" ? false:true;
-                //     TW_Log("TW_DATA_KEY.LobbyReadyOK---"+ret,TW_Store.gameUpateStore.isNeedUpdate)
-                // }
+                if(TW_Store.dataStore.isAppInited){
+                    if(ret){
+                        TW_Store.gameUpateStore.isNeedUpdate=`${ret}` == "1" ? false:true;
+                    }
+                    TW_Log("TW_DATA_KEY.LobbyReadyOK---"+ret,TW_Store.gameUpateStore.isNeedUpdate)
+                }
             });
         });
 
