478496e0578267b7ab0d66a9bcd460c39aaf0a69
chun <chun-jx@internet.com>
2019-12-11 16:26:07 +0800

    autoMerge origin/release/develop date=1211.1626

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -37,7 +37,7 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐';
 
-export const versionHotFix = 'v4.1211.1618';
+export const versionHotFix = 'v4.1211.1626';
 
 export const MyOwnerPlatName= '博博乐';
 
@@ -72,23 +72,22 @@ export const platInfo = {
     brand: "qp01",
     latestNativeVersion:{ios:"10",android:"10"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
     appInfo : {
-        ch_8:{
-            name:"appstoreWithCodePush",
-            wxAppKey:"wx14a4c6864a801156",
-            wxAppSecret:"06f5151830e538583897772e402ae142",
-            JPushKey: "a09286d3570274effabdab10",
+        ch_8: {
+            name: "appstoreWithCodePush",
+            wxAppKey: "wx26019a5a7de8db15",
+            wxAppSecret: "62ef8d30f3c4e244bde8831dd2f4ec84",
+            JPushKey: "e9f6454032a3f4936d80a82f",
             UmengKey: "5b9f7642f43e486308000111",
-            openInstallKey: "k9e92p"
+            openInstallKey: "evna9i"
         },
-        ch_9:{
-            name:"appstoreNoCodePush",
-            wxAppKey:"wx14a4c6864a801156",
-            wxAppSecret:"06f5151830e538583897772e402ae142",
-            JPushKey: "a09286d3570274effabdab10",
+        ch_9: {
+            name: "appstoreNoCodePush",
+            wxAppKey: "wx26019a5a7de8db15",
+            wxAppSecret: "62ef8d30f3c4e244bde8831dd2f4ec84",
+            JPushKey: "e9f6454032a3f4936d80a82f",
             UmengKey: "5b9f7642f43e486308000111",
-            openInstallKey: "k9e92p"
+            openInstallKey: "evna9i"
         },
-
     }
 }
 
