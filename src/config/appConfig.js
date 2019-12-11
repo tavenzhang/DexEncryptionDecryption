dd876b8fb9ee1e0dbbb1a48356137e7eac3089ca
chun <chun-jx@internet.com>
2019-12-11 16:26:05 +0800

    autoMerge origin/release/develop date=1211.1626

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -38,7 +38,7 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐sit';
 
-export const versionHotFix = 'v4.1211.1607';
+export const versionHotFix = 'v4.1211.1626';
 
 export const MyOwnerPlatName= 'sit';
 
@@ -60,6 +60,24 @@ export const platInfo = {
     platId: configAppId,
     brand: "sqp01",
     latestNativeVersion:{ios:"12",android:"12"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
+    appInfo : {
+        ch_8: {
+            name: "appstoreWithCodePush",
+            wxAppKey: "wx14a4c6864a801156",
+            wxAppSecret: "06f5151830e538583897772e402ae142",
+            JPushKey: "a09286d3570274effabdab10",
+            UmengKey: "5b9f7642f43e486308000111",
+            openInstallKey: "k9e92p"
+        },
+        ch_9: {
+            name: "appstoreNoCodePush",
+            wxAppKey: "wx14a4c6864a801156",
+            wxAppSecret: "06f5151830e538583897772e402ae142",
+            JPushKey: "a09286d3570274effabdab10",
+            UmengKey: "5b9f7642f43e486308000111",
+            openInstallKey: "k9e92p"
+        },
+    }
 
 }
 
