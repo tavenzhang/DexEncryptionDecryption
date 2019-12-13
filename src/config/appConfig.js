7092dc6008c86a75c7fc4a18b676c8be97ad8fb0
chun <chun-jx@internet.com>
2019-12-13 10:32:57 +0800

    autoMerge origin/release/release/uat date=1213.1032

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -37,7 +37,7 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐';
 
-export const versionHotFix = 'v5.1213.0942';
+export const versionHotFix = 'v5.1213.1032';
 
 export const MyOwnerPlatName= '博博乐';
 
