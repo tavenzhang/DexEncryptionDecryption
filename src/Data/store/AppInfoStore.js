7e1d15ca9447afa58acb58944e82e209c392d964
administrator <administrator@Catalina.local>
2020-02-24 19:24:48 -0800

    autoMerge origin/release/uat date=0224.1924

--- src/Data/store/AppInfoStore.js
+++ src/Data/store/AppInfoStore.js
@@ -58,7 +58,7 @@ export default class AppInfoStore {
      */
     userAffCode = '';
     @observable
-    specialVersionHot = '';
+    specialVersionHot = '5';
 
     @observable
     versionHotFix = versionHotFix;
