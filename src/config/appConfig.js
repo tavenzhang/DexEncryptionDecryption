a43550393d1f4bccdc97f5d972a77f105b21c20d
administrator <administrator@Catalina.local>
2020-05-25 22:24:41 +0800

    autoMerge origin/release/uat date=0525.2224

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -38,7 +38,7 @@ export const AppConfig = {
 
 export const MyAppName = '棋牌sit';
 
-export const versionHotFix = 'v5.0525.2141';
+export const versionHotFix = 'v5.0525.2224';
 
 export const MyOwnerPlatName= 'sit';
 
