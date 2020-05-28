289ae1fcf45cd476e5e2162b972fdf091533d74a
administrator <administrator@Catalina.local>
2020-05-28 15:06:53 +0800

    autoMerge origin/release/uat date=0528.1506

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -36,9 +36,9 @@ export const AppConfig = {
 
 export const MyAppName = '博必胜棋牌';
 
-export const versionHotFix = 'v5.0528.1223';
+export const MyOwnerPlatName= '博必胜棋牌';
 
-export const versionHotFix = 'v5.0528.1223';
+export const versionHotFix = 'v5.0528.1506';
 
 //第三方安全域名base64 key
 export const safeguardKey = 'ewogICAgImQiOlsKICAgICAgICAiaHR0cHM6Ly8yZDQxMDhlOGVmNTNhNThmNzRiNTQ1YmMwYWU0NjgyZi5vc3MtY24tc2hlbnpoZW4uYWxpeXVuY3MuY29tIiwKICAgICAgICAiaHR0cHM6Ly8yZDQxMDhlOGVmNTNhNThmNzRiNTQ1YmMwYWU0NjgyZi5zMy1hY2NlbGVyYXRlLmFtYXpvbmF3cy5jb20iLAogICAgICAgICJodHRwczovLzJkNDEwOGU4ZWY1M2E1OGY3NGI1NDViYzBhZTQ2ODJmLmF6dXJlZWRnZS5uZXQiCiAgICBdCn0='
