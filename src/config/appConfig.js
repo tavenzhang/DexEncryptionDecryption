7da77c34bd74ca78adcd7eecfbc2bcd847b5d96a
administrator <administrator@Catalina.local>
2020-06-02 14:33:24 +0800

    autoMerge origin/release/uat date=0602.1433

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -38,7 +38,7 @@ export const AppConfig = {
 
 export const MyAppName = '棋牌sit';
 
-export const versionHotFix = 'v6.0602.1413';
+export const versionHotFix = 'v6.0602.1433';
 
 export const MyOwnerPlatName= 'sit';
 
@@ -51,7 +51,7 @@ export const safeguardKey = 'eyJkIjogWyJodHRwczovLzk4NzY0NWJhMDBhOWIwNDE2YjI1NGY
 
 export const platInfo = {
 
-    downDomain:"https://download.sufangxingwanshisanjiao.com",
+    loadHintColor:"#0066a6",
 
     zipCheckServer: {
         debug_server: "http://192.168.14.70:8888",
