e96944506f9cebc0af9b84d8d3783192aa2c2df3
258333601@163.com <258333601@163.com>
2020-06-09 21:29:36 +0400

    微信登陆反馈提示

--- src/Data/store/BBLStore.js
+++ src/Data/store/BBLStore.js
@@ -459,10 +459,6 @@ export default class BBLStore {
                                     "\n\n==> wxLogin checkWXInstall---" + ret;
                                 if (ret) {
                                     TN_WechatAuth((code, result, message) => {
-                                        TW_Store.bblStore.getWebAction(
-                                            TW_Store.bblStore.ACT_ENUM.popTip,
-                                            {data: "微信授权 error="+code +" result="+result+"--message--"+message}
-                                        )
                                         //TW_Store.dataStore.log += "\n==> wxLogin message---" + JSON.stringify(result) + "---\n--code====" + code + "===message==" + message;
                                         TW_Log(
                                             'wxLogin code----' + code + '---message---' + message,
