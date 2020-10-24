bf44cb6d26942115f11e4f9f7cef987c224a74da
administrator <administrator@Catalina.local>
2020-10-25 02:26:28 +0800

    autoMerge origin/release/uat date=1025.0226

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -37,7 +37,7 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐';
 
-export const versionHotFix = 'v6.1025.0224';
+export const versionHotFix = 'v6.1025.0226';
 
 export const MyOwnerPlatName= '博博乐';
 
